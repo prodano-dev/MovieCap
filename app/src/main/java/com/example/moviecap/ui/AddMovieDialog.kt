443c2feb2ee360da909b5fb2ba978ac763f836b8
Danny <danny.pro17@gmail.com>
2020-12-26 12:22:38 +0100

    User can make an image of the movie cover.

--- app/src/main/java/com/example/moviecap/ui/AddMovieDialog.kt
+++ app/src/main/java/com/example/moviecap/ui/AddMovieDialog.kt
@@ -1,7 +1,11 @@
 package com.example.moviecap.ui
 
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +13,12 @@ import androidx.fragment.app.DialogFragment
 import com.example.moviecap.R
 import kotlinx.android.synthetic.main.fragment_add_movie.*
 
+
 class AddMovieDialog: DialogFragment() {
+
+    private val PERMISSION_CODE = 1000;
+    private val IMAGE_CAPTURE_CODE = 1001
+    var image_uri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +38,29 @@ class AddMovieDialog: DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        testButton.setOnClickListener {
+            openCamera()
+        }
+    }
 
+    private fun openCamera() {
+        val resolver = requireActivity().contentResolver
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK){
+            //set image captured to image view
+            movieImageView.setImageURI(image_uri)
+        }
     }
 }
