8cc40834621d13e9e52d773b9f3ebc7579712aa9
Danny <danny.pro17@gmail.com>
2020-12-26 11:50:45 +0100

    Made a begin to add own movies.

---
+++ app/src/main/java/com/example/moviecap/ui/AddMovieDialog.kt
@@ -0,0 +1,34 @@
+package com.example.moviecap.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.moviecap.R
+import kotlinx.android.synthetic.main.fragment_add_movie.*
+
+class AddMovieDialog: DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_movie, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
+        val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
+        dialog!!.window?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
--- app/src/main/java/com/example/moviecap/ui/dashboard/SearchMovieFragment.kt
+++ app/src/main/java/com/example/moviecap/ui/dashboard/SearchMovieFragment.kt
@@ -1,25 +1,31 @@
 package com.example.moviecap.ui.dashboard
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.camera.core.ImageCapture
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResult
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.example.moviecap.model.MovieDB
 import com.example.moviecap.R
-import com.example.moviecap.viewModel.MoveDBViewModel
+import com.example.moviecap.model.MovieDB
+import com.example.moviecap.ui.AddMovieDialog
 import com.example.moviecap.ui.home.BUNDLE_MOVIE_KEY
 import com.example.moviecap.ui.home.MovieAdapter
 import com.example.moviecap.ui.home.REQ_MOVIE_KEY
+import com.example.moviecap.viewModel.MoveDBViewModel
 import kotlinx.android.synthetic.main.fragment_search_movie.*
+import java.io.File
+import java.util.concurrent.ExecutorService
 
 class SearchMovieFragment : Fragment() {
 
@@ -27,6 +33,11 @@ class SearchMovieFragment : Fragment() {
     private val viewModel: MoveDBViewModel by activityViewModels()
     private val searchedMovies = arrayListOf<MovieDB>()
     private val searchedMoviesAdapter = MovieAdapter(searchedMovies) { onMovieClick(it)}
+
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outPutDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -41,7 +52,6 @@ class SearchMovieFragment : Fragment() {
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
-
             override fun onQueryTextChange(newText: String): Boolean {
 
                 if (newText == ""){
@@ -49,18 +59,42 @@ class SearchMovieFragment : Fragment() {
                 } else {
                     viewModel.searchMovies(newText)
                 }
-
                 return false
             }
 
             override fun onQueryTextSubmit(p0: String?): Boolean {
                 TODO("Not yet implemented")
             }
-
         })
+
 
         initView()
         observeMovies()
+        filterButton.setOnClickListener {
+            showDialog()
+        }
+
+    }
+
+    private fun showDialog() {
+        val fm = fragmentManager
+        val editNameDialogFragment = AddMovieDialog()
+        editNameDialogFragment.setTargetFragment(this@SearchMovieFragment, 300)
+        editNameDialogFragment.show(fm!!, "fragment_edit_name")
+//        val builder = AlertDialog.Builder(requireContext())
+//        builder.setTitle("Add movie")
+//        imageCapture = ImageCapture.Builder().build()
+//
+//        val dialogLayout = layoutInflater.inflate(R.layout.fragment_add_movie, null)
+//        builder.setView(dialogLayout)
+//        builder.setPositiveButton("ADD") { _: DialogInterface, _: Int ->
+//
+//
+//        }
+//        builder.setNegativeButton("CANCEL") {dialogInterface, which ->
+//            Toast.makeText(context, "NEE", Toast.LENGTH_LONG).show()
+//        }
+//        builder.show()
 
     }
 
@@ -80,7 +114,12 @@ class SearchMovieFragment : Fragment() {
             searchedMovies.addAll(it)
             searchedMoviesAdapter.notifyDataSetChanged()
         })
-
     }
 
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
 }
